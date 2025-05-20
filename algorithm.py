5e9609aebaae8e1aa795098252695864b3048b66
scottmsoh <oms870728@gmail.com>
2025-05-19 18:10:52 -0700

    feat: Write the code for knnclassifier

--- algorithm.py
+++ algorithm.py
@@ -307,4 +307,28 @@ class KMeans:
         distances = np.linalg.norm(X[:, np.newaxis] - self.centroids, axis=2)
         return np.argmin(distances, axis=1)
 
-       return y_pred
+
+class KNNClassifier:
+    def __init__(self, k=3):
+        self.k = k
+        self.X_train = None
+        self.y_train = None
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def _predict_single(self, x):
+        distances = np.linalg.norm(self.X_train-x, axis=1)
+        k_indices = np.argsort(distances)[:self.k]
+        k_labels = self.y_train[k_indices]
+
+        return np.argmax(np.bincount(k_labels))
+
+    def predict(self, X):
+        return np.array([self._predict_single(x) for x in X])
+
+    def score(self, X, y):
+        y_pred = self.predict(X)
+
+        return np.mean(y_pred == y)
