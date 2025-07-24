421885e097d1c2ec5a686a511c8ba4b00512b017
scottmsoh <oms870728@gmail.com>
2025-07-23 17:57:55 -0700

    feat: Write code regarding data preprocessing
    Write code preprocessing code for setting training dataset(target variable:alive, too many missing data points), dropping null values, enabling label encoder, and spliting train and test dataset.
    
    Ticket: KAN-9

--- dags/titanic_dag.py
+++ dags/titanic_dag.py
@@ -4,6 +4,8 @@ import pandas as pd
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 
 default_args = {
@@ -23,13 +25,40 @@ dag = DAG(
     schedule=timedelta(days=1),
 )
 
-def data_collection():
+
+def data_collection(**kwargs):
     data = pd.read_csv('/opt/airflow/dags/titanic.csv')
     return data
 
+
+def data_preprocessing(**kwargs):
+    data = pd.read_csv('/opt/airflow/dags/titanic.csv')
+    data = data.drop(columns=['deck', 'alive'])
+    data = data.dropna()
+    cat_cols = data.select_dtypes(include=['object', 'category']).columns
+
+    le = LabelEncoder()
+    for col in cat_cols:
+        data[col] = data[col].astype(str)
+        data[col] = le.fit_transform(data[col])
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        data.drop(columns='survived'), data.survived, test_size=0.2, random_state=42
+    )
+    # return X_train, X_test, y_train, y_test
+
 data = PythonOperator(
-    task_id=f'data',
+    task_id='data_collection',
     python_callable=data_collection,
     # op_kwargs={'word': word},
     dag=dag,
 )
+
+preprocessed_data = PythonOperator(
+    task_id='data_preprocessing',
+    python_callable=data_preprocessing,
+    # op_kwargs={'data': data},
+    dag=dag,
+)
+
+data >> preprocessed_data
